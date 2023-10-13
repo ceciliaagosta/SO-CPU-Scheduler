8dcba70059601b5265d269e0ca2b8a3301aa48ab
ceciliaagosta <139223682+ceciliaagosta@users.noreply.github.com>
2023-10-13 18:37:49 +0200

    Cleaned the code a bit
    Nothing new, just cleaned the code and added a count of extractions for each number.

--- probability_sample.c
+++ probability_sample.c
@@ -4,15 +4,12 @@
 #include "probability_distribution.h"
 
 #define print_array(data, n) _Generic((*data), int: print_int, double: print_double)(data,n)
-
-
 void print_int (const int* data, size_t n) {
     for(size_t i=0; i<n; i++) {
         printf("%d ", data[i]);
     }
     printf("\n");
 }
-
 void print_double (const double* data, size_t n) {
     for(size_t i=0; i<n; i++) {
         printf("%.3lf ", data[i]);
@@ -20,17 +17,15 @@ void print_double (const double* data, size_t n) {
     printf("\n");
 }
 
+
 //Function to generate random numbers between 0 and 1, in a uniform distribution
 double unif_rand() {
     return (double) rand() / RAND_MAX;
 }
 
+
 //Function to generate n CPU bursts and m IO bursts based on the given ProbHistogram h. Saves them in the given arrays. Uses the cumulative probabilities method
-int* CDF_generator(ProbHistogram* h, int* CPUres, int* IOres, int n, int m) {
-    
-    int done[2];
-    done[0] = 0;
-    done[1] = 0;
+void CDF_generator(ProbHistogram* h, int* CPUres, int* IOres, int n, int m) {
     
     //Check if given probabilities sum up to 1
     int CPUcheck = ProbDist_checkCPU(h);
@@ -38,25 +33,23 @@ int* CDF_generator(ProbHistogram* h, int* CPUres, int* IOres, int n, int m) {
     if (CPUcheck < 0 || IOcheck < 0) {
         printf("Sum of probabilities doesn't amount to 1 in either the CPU or the IO histograms.\n");
         printf("Incorrect sum in: %s  %s\n", CPUcheck<0?"CPU":"--", IOcheck<0?"IO":"--");
-        return done;
+        return;
     }
     
-    //Generate the cumulative probabilities array
-    //Generate a random number between 0 and 1
-    //Generate integer and save in right array
+    //Generate the cumulative probabilities array for CPU
     int max_duration = h->max_duration + 1;
     double random;
     
     if (CPUcheck) {
         double* CPUprobs_sum = (double*) malloc(max_duration * sizeof(double));
         CPUprobs_sum[0] = 0;
-        printf("CPU cumulative probabilities: ");
+        //printf("CPU cumulative probabilities: ");
         for (int i=1; i<max_duration; i++) {
             CPUprobs_sum[i] += CPUprobs_sum[i-1] + h->CPUprobs[i];
-            printf("%.3lf ", CPUprobs_sum[i]);
+            //printf("%.3lf ", CPUprobs_sum[i]);
         }
-        printf("\n");
-        
+        //printf("\n");
+        //Generate integer and save in right array
         for (int i=0; i<n; i++) {
             random = unif_rand();
             for (int j=1; j<max_duration; j++) {
@@ -66,21 +59,20 @@ int* CDF_generator(ProbHistogram* h, int* CPUres, int* IOres, int n, int m) {
                 }
             }
         }
-        done[0] = 1;
         free(CPUprobs_sum);
     }
-    
+    //Generate the cumulative probabilities array for IO
     if (IOcheck) {
         double* IOprobs_sum = (double*) malloc(max_duration * sizeof(double));
         IOprobs_sum[0] = 0;
-        printf("IO cumulative probabilities: ");
+        //printf("IO cumulative probabilities: ");
         for (int i=1; i<max_duration; i++) {
             IOprobs_sum[i] += IOprobs_sum[i-1] + h->IOprobs[i];
-            printf("%.3lf ", IOprobs_sum[i]);
+            //printf("%.3lf ", IOprobs_sum[i]);
         }
-        printf("\n");
-        
-        for (int i=0; i<n; i++) {
+        //printf("\n");
+        //Generate integer and save in right array
+        for (int i=0; i<m; i++) {
             random = unif_rand();
             for (int j=1; j<max_duration; j++) {
                 if (random <= IOprobs_sum[j]) {
@@ -89,11 +81,10 @@ int* CDF_generator(ProbHistogram* h, int* CPUres, int* IOres, int n, int m) {
                 }
             }
         }
-        done[1] = 1;
         free(IOprobs_sum);
     }
     
-    return done;
+    return;
     
 }
 
@@ -126,19 +117,17 @@ int main(int argc, char** argv) {
     for (int i=0; i<CPUnum; i++) CPUbursts[i] = 0;
     for (int i=0; i<IOnum; i++) IObursts[i] = 0;
     
-    int* done = CDF_generator(&h, CPUbursts, IObursts, CPUnum, IOnum);
+    CDF_generator(&h, CPUbursts, IObursts, CPUnum, IOnum);
     
-    printf("Done: %d %d\n", done[0], done[1]);
-    
-    if (done[0]) {
+    /*if (ProbDist_checkCPU(&h) == 1) {
         printf("CPU bursts: ");
         print_array(CPUbursts, CPUnum);
     }
     
-    if (done[1]) {
+    if (ProbDist_checkIO(&h) == 1) {
         printf("IObursts: ");
         print_array(IObursts, IOnum);
-    }
+    }*/
     
     int max_duration = h.max_duration + 1;
     
@@ -156,8 +145,17 @@ int main(int argc, char** argv) {
         IOcompare[IObursts[i]]++;
     }
     
-    print_array(CPUcompare, max_duration);
-    print_array(IOcompare, max_duration);
+    //print_array(CPUcompare, max_duration);
+    //print_array(IOcompare, max_duration);
+    
+    for (int i=1; i<max_duration; i++) {
+        
+        float cpu = (float)CPUcompare[i] / CPUnum;
+        float io = (float)IOcompare[i] / IOnum;
+        
+        printf("Duration: %d --> CPU Percentage: %.3lf || CPU Probability: %.3lf\n", i, cpu, h.CPUprobs[i]);
+        printf("                IO Percentage:  %.3lf || IO Probability:  %.3lf\n\n", io, h.IOprobs[i]);
+    }
     
     free(CPUbursts);
     free(IObursts);
